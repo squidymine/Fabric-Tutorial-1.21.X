e4f937af92ea5d976edd58a6774bfbac6c2d4afc
squidymine <7squidymine@gmail.com>
2025-06-08 00:17:54 -0400

    V1.03 -Added support system for custom recipes -Added support system for custom loot tables -Added 4 recipes -Added 4 loot tables -Added 2 new blocks

--- src/main/java/com/squidymine/tutorialmod/block/ModBlocks.java
+++ src/main/java/com/squidymine/tutorialmod/block/ModBlocks.java
@@ -4,6 +4,8 @@ import com.squidymine.tutorialmod.TutorialMod;
 import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.block.AbstractBlock;
 import net.minecraft.block.Block;
+import net.minecraft.block.ExperienceDroppingBlock;
+import net.minecraft.client.gl.Uniform;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroups;
@@ -11,6 +13,7 @@ import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 
 public class ModBlocks {
     public static final Block PINK_GARNET_BLOCK = registerBlock("pink_garnet_block",
@@ -21,6 +24,15 @@ public class ModBlocks {
             new Block(AbstractBlock.Settings.create().strength(3f)
                     .requiresTool()));
 
+    public static final Block PINK_GARNET_ORE = registerBlock("pink_garnet_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(2, 5),
+                    AbstractBlock.Settings.create().strength(3f).requiresTool()));
+
+    public static final Block PINK_GARNET_DEEPSLATE_ORE = registerBlock("pink_garnet_deepslate_ore",
+            new ExperienceDroppingBlock(UniformIntProvider.create(3, 6),
+                    AbstractBlock.Settings.create().strength(4f).requiresTool().sounds(BlockSoundGroup.DEEPSLATE)));
+
+
 
     private static Block registerBlock(String name, Block block) {
         registerBlockItem(name, block);
@@ -29,7 +41,7 @@ public class ModBlocks {
 
     private static void registerBlockItem(String name, Block block) {
         Registry.register(Registries.ITEM, Identifier.of(TutorialMod.MOD_ID, name),
-                new BlockItem(block, new Item.Settings())); // TEST OUT WHAT THE BlockItem.Settings() ARE
+                new BlockItem(block, new Item.Settings())); // BlockItem.Settings() might just be the same as Item.Settings()???
     }
 
     public static void registerModBlocks() {
