bfb4c165d13fe31a64a18820f94205ef50d626f9
squidymine <7squidymine@gmail.com>
2025-06-18 00:46:59 -0400

    V1.19 - Custom Effects -Added Custom Effect Support -Added 1 New Custom Effect

--- src/main/java/com/squidymine/tutorialmod/TutorialMod.java
+++ src/main/java/com/squidymine/tutorialmod/TutorialMod.java
@@ -2,6 +2,7 @@ package com.squidymine.tutorialmod;
 
 import com.squidymine.tutorialmod.block.ModBlocks;
 import com.squidymine.tutorialmod.component.ModDataComponentTypes;
+import com.squidymine.tutorialmod.effect.ModEffects;
 import com.squidymine.tutorialmod.item.ModItemGroups;
 import com.squidymine.tutorialmod.item.ModItems;
 import com.squidymine.tutorialmod.sound.ModSounds;
@@ -33,6 +34,8 @@ public class TutorialMod implements ModInitializer {
 
 		ModDataComponentTypes.registerDataComponentTypes();
 		ModSounds.registerSounds();
+
+		ModEffects.registerEffects();
 
 		// Can create a custom class of all fuels that has a static register method that can be called here
 		FuelRegistry.INSTANCE.add(ModItems.STARLIGHT_ASHES, 600);
---
+++ src/main/java/com/squidymine/tutorialmod/effect/ModEffects.java
@@ -0,0 +1,32 @@
+package com.squidymine.tutorialmod.effect;
+
+import com.squidymine.tutorialmod.TutorialMod;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffect;
+import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.util.Identifier;
+
+// Effect Textures ("mob_effect") are 18x18
+
+public class ModEffects {
+    public static final RegistryEntry<StatusEffect> SLIMEY = registerStatusEffect("slimey",
+            new SlimeyEffect(StatusEffectCategory.NEUTRAL, 0x36ebab)
+                    .addAttributeModifier(EntityAttributes.GENERIC_MOVEMENT_SPEED,
+                            Identifier.of(TutorialMod.MOD_ID, "slimey"), -0.25f,
+                            EntityAttributeModifier.Operation.ADD_MULTIPLIED_TOTAL));
+
+
+
+    private static RegistryEntry<StatusEffect> registerStatusEffect(String name, StatusEffect statusEffect) {
+        return Registry.registerReference(Registries.STATUS_EFFECT, Identifier.of(TutorialMod.MOD_ID, name), statusEffect);
+    }
+
+    public static void registerEffects() {
+        TutorialMod.LOGGER.info("Registering Mod Effects for " + TutorialMod.MOD_ID);
+    }
+}
