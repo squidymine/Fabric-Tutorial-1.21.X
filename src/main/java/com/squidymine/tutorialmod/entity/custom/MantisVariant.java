01731e122c6b6908643906bb92c7576af25a631d
squidymine <7squidymine@gmail.com>
2025-06-24 16:17:57 -0400

    V1.29 - Custom Entity Variant -Added 1 New Variant to 1 Custom Entity

---
+++ src/main/java/com/squidymine/tutorialmod/entity/custom/MantisVariant.java
@@ -0,0 +1,27 @@
+package com.squidymine.tutorialmod.entity.custom;
+
+import java.util.Arrays;
+import java.util.Comparator;
+
+//LOOK AT HorseColor FOR A GOOD REFERENCE :)
+
+public enum MantisVariant {
+    DEFAULT(0),
+    ORCHID(1);
+
+    private static final MantisVariant[] BY_ID = Arrays.stream(values()).sorted(Comparator.
+            comparingInt(MantisVariant::getId)).toArray(MantisVariant[]::new);
+    private final int id;
+
+    MantisVariant(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public static MantisVariant byId(int id) {
+        return BY_ID[id % BY_ID.length];
+    }
+}
