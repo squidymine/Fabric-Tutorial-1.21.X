076f043ddf17e6443b0631ac372089cb64df76d2
squidymine <7squidymine@gmail.com>
2025-06-24 16:37:03 -0400

    V1.31 - Custom Entity Sounds -Added Sounds to 1 Custom Entity

--- src/main/java/com/squidymine/tutorialmod/entity/custom/MantisEntity.java
+++ src/main/java/com/squidymine/tutorialmod/entity/custom/MantisEntity.java
@@ -9,6 +9,7 @@ import net.minecraft.entity.SpawnReason;
 import net.minecraft.entity.ai.goal.*;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
 import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.data.DataTracker;
 import net.minecraft.entity.data.TrackedData;
 import net.minecraft.entity.data.TrackedDataHandlerRegistry;
@@ -20,6 +21,8 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NbtCompound;
 import net.minecraft.recipe.Ingredient;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundEvent;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.Util;
 import net.minecraft.world.LocalDifficulty;
 import net.minecraft.world.ServerWorldAccess;
@@ -134,5 +137,23 @@ public class MantisEntity extends AnimalEntity {
         MantisVariant variant = Util.getRandom(MantisVariant.values(), this.random); // gives a random variant
         setVariant(variant);
         return super.initialize(world, difficulty, spawnReason, entityData);
+    }
+
+    /* SOUNDS */
+    // You generally want to override methods that return a SoundEvent
+
+    @Override
+    protected @Nullable SoundEvent getAmbientSound() {
+        return SoundEvents.ENTITY_PARROT_AMBIENT;
+    }
+
+    @Override
+    protected @Nullable SoundEvent getHurtSound(DamageSource source) {
+        return SoundEvents.ENTITY_ALLAY_HURT;
+    }
+
+    @Override
+    protected @Nullable SoundEvent getDeathSound() {
+        return SoundEvents.ENTITY_PANDA_DEATH;
     }
 }
