daf6c827a26b814f3f084ac1937f5f676b0242d2
squidymine <7squidymine@gmail.com>
2025-06-28 20:21:04 -0400

    V1.37 - Spawning Particles -Added Particle Effects to 1 Custom Item

--- src/main/java/com/squidymine/tutorialmod/item/custom/ChiselItem.java
+++ src/main/java/com/squidymine/tutorialmod/item/custom/ChiselItem.java
@@ -12,6 +12,9 @@ import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.ItemUsageContext;
 import net.minecraft.item.tooltip.TooltipType;
+import net.minecraft.particle.BlockStateParticleEffect;
+import net.minecraft.particle.ParticleType;
+import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.SoundCategory;
@@ -50,6 +53,14 @@ public class ChiselItem extends Item {
                         item -> context.getPlayer().sendEquipmentBreakStatus(item, EquipmentSlot.MAINHAND)); // Item getting damaged
 
                 world.playSound(null, context.getBlockPos(), ModSounds.CHISEL_USE, SoundCategory.BLOCKS);
+
+                ((ServerWorld) world).spawnParticles(new BlockStateParticleEffect(ParticleTypes.BLOCK, clickedBlock.getDefaultState()), //Break block particles
+                        context.getBlockPos().getX() + 0.5, context.getBlockPos().getY() + 1.0,
+                        context.getBlockPos().getZ() + 0.5, 5, 0, 0, 0, 1); // delta is direction of the speed (prob)
+
+                ((ServerWorld) world).spawnParticles(ParticleTypes.FLAME, // Diff particle types might have their own speeds/directions
+                        context.getBlockPos().getX() + 0.5, context.getBlockPos().getY() + 1.5,
+                        context.getBlockPos().getZ() + 0.5, 10, 0, 0, 0, 3);
 
                 context.getStack().set(ModDataComponentTypes.COORDINATES, context.getBlockPos()); // Can pass in null to delete the data
                 //context.getStack().set(DataComponentTypes. , );
