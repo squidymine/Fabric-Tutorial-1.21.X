88409eede3b6aa6859efffc895b1460eccfd7abb
squidymine <7squidymine@gmail.com>
2025-06-30 23:59:56 -0400

    V1.44 - REI Compatibility -Added new REI Crafting Group

---
+++ src/main/java/com/squidymine/tutorialmod/compat/GrowthChamberDisplay.java
@@ -0,0 +1,23 @@
+package com.squidymine.tutorialmod.compat;
+
+import com.squidymine.tutorialmod.recipe.GrowthChamberRecipe;
+import me.shedaniel.rei.api.common.category.CategoryIdentifier;
+import me.shedaniel.rei.api.common.display.basic.BasicDisplay;
+import me.shedaniel.rei.api.common.entry.EntryIngredient;
+import me.shedaniel.rei.api.common.util.EntryIngredients;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+import net.minecraft.recipe.RecipeEntry;
+
+import java.util.List;
+
+public class GrowthChamberDisplay extends BasicDisplay {
+    public GrowthChamberDisplay(RecipeEntry<GrowthChamberRecipe> recipe) {
+        super(List.of(EntryIngredients.ofIngredient(recipe.value().getIngredients().get(0))),
+                List.of(EntryIngredient.of(EntryStacks.of(recipe.value().getResult(null)))));
+    }
+
+    @Override
+    public CategoryIdentifier<?> getCategoryIdentifier() {
+        return GrowthChamberCategory.GROWTH_CHAMBER;
+    }
+}
---
+++ src/main/java/com/squidymine/tutorialmod/compat/TutorialModREIClient.java
@@ -0,0 +1,34 @@
+package com.squidymine.tutorialmod.compat;
+
+import com.squidymine.tutorialmod.block.ModBlocks;
+import com.squidymine.tutorialmod.recipe.GrowthChamberRecipe;
+import com.squidymine.tutorialmod.recipe.ModRecipes;
+import com.squidymine.tutorialmod.screen.custom.GrowthChamberScreen;
+import me.shedaniel.math.Rectangle;
+import me.shedaniel.rei.api.client.plugins.REIClientPlugin;
+import me.shedaniel.rei.api.client.registry.category.CategoryRegistry;
+import me.shedaniel.rei.api.client.registry.display.DisplayRegistry;
+import me.shedaniel.rei.api.client.registry.screen.ScreenRegistry;
+import me.shedaniel.rei.api.common.util.EntryStacks;
+
+public class TutorialModREIClient implements REIClientPlugin {
+    @Override
+    public void registerCategories(CategoryRegistry registry) {
+        registry.add(new GrowthChamberCategory());
+
+        registry.addWorkstations(GrowthChamberCategory.GROWTH_CHAMBER, EntryStacks.of(ModBlocks.GROWTH_CHAMBER));
+    }
+
+    @Override
+    public void registerDisplays(DisplayRegistry registry) {
+        registry.registerRecipeFiller(GrowthChamberRecipe.class, ModRecipes.GROWTH_CHAMBER_TYPE,
+                GrowthChamberDisplay::new);
+    }
+
+    @Override
+    public void registerScreens(ScreenRegistry registry) {
+        registry.registerClickArea(screen -> new Rectangle(((screen.width - 176) / 2) + 78,
+                        ((screen.height - 166) / 2) + 30, 20, 25), GrowthChamberScreen.class,
+                GrowthChamberCategory.GROWTH_CHAMBER);
+    }
+}
